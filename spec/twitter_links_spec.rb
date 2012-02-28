435fc1e4ec6aac9a8064f82bae956c0c11ad2819
Siva Kumar <siva.rm@gmail.com>
2012-02-28 16:32:46 -0500

    basic initializer

--- spec/twitter_links_spec.rb
+++ spec/twitter_links_spec.rb
@@ -2,22 +2,46 @@ require "spec_helper"
 
 describe TwitterLinks do
 
+  let(:linkies) { %w{A B C D E} }
+
   context "Setup" do
 
-    it "has a tweets variable" do
-      t = TwitterLinks.new()
+    before :each do
+      @t = TwitterLinks.new()
+    end
 
-      t.tweets = "Tweets"
-      t.tweets.should eql "Tweets"
+    it "has a tweets variable" do
+      @t        = TwitterLinks.new()
+      @t.tweets = "Tweets"
+      @t.tweets.should eql "Tweets"
     end
 
     it "has a hashtag variable" do
-      t = TwitterLinks.new()
-
-      t.hashtag = "#ruby"
-      t.hashtag.should eql "#ruby"
+      @t         = TwitterLinks.new()
+      @t.hashtag = "#ruby"
+      @t.hashtag.should eql "#ruby"
     end
 
+    it "has a links variable" do
+      @t       = TwitterLinks.new()
+      links    = %w{A B C D E}
+      @t.links = links
+      @t.links.should eql links
+    end
+
+    it "supports setting attributes in the initializer" do
+      t = TwitterLinks.new(tweets: "tweets", hashtag: "#ruby", links: linkies)
+      t.tweets.should eql "tweets"
+      t.hashtag.should eql "#ruby"
+      t.links.should eql linkies
+    end
+  end
+
+  context "HashTag" do
+    it "stores the given hashtag" do
+      ruby = TwitterLinks.new(hashtag: "#ruby")
+      ruby.hashtag.should eql "#ruby"
+    end
   end
 
   context "Search" do
--- twitter_links.rb
+++ twitter_links.rb
@@ -1,7 +1,12 @@
 class TwitterLinks
 
-  attr_accessor :tweets, :hashtag
-  
+  attr_accessor :tweets, :hashtag, :links
+
+  def initialize(attrs={ })
+    attrs.each do |k, v|
+      send("#{k}=", v)
+    end
+  end
 end
 
 
